2759a11df79e64a01836b3c38fe2ec00a90944ac
Vlad-Buko <vladislav_buko@mail.ru>
2022-09-27 01:03:37 +0300

    Up

---
+++ src/main/java/com/example/testapigatewy/TestApiGatewyApplication.java
@@ -0,0 +1,13 @@
+package com.example.testapigatewy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApiGatewyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApiGatewyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testapigatewy/TestApiGatewyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testapigatewy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApiGatewyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
